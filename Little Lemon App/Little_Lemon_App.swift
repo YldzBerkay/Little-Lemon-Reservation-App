b85b6d68afaa0f4a2a1caa4ff66bcb545b1ee489
Berkay Yıldız <berkay4421@gmail.com>
2023-06-16 21:08:39 +0300

    final commit

---
+++ Little Lemon App/Little_Lemon_App.swift
@@ -0,0 +1,17 @@
+//
+//  Little_Lemon_AppApp.swift
+//  Little Lemon App
+//
+//  Created by Berkay Yıldız on 16.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Little_Lemon_App: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
